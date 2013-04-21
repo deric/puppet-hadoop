452f5fe5b6e48270f231874698e4d36cf19dc80b
Tomas Barton <barton.tomas@gmail.com>
2013-04-21 21:58:53 +0200

    hadoop installation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,41 +1,16 @@
-# == Class: hadoop
+# Class: hadoop
 #
-# Full description of class hadoop here.
+# This module manages hadoop
 #
-# === Parameters
+# Parameters: None
 #
-# Document parameters here.
+# Actions: None
 #
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
+# Requires: None
 #
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if it
-#   has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should not be used in preference to class parameters  as of
-#   Puppet 2.6.)
-#
-# === Examples
-#
-#  class { hadoop:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ]
-#  }
-#
-# === Authors
-#
-# Author Name <author@domain.com>
-#
-# === Copyright
-#
-# Copyright 2013 Your name here, unless otherwise noted.
+# Sample Usage: include hadoop
 #
 class hadoop {
-
-
+  include hadoop::install
+  include hadoop::config
 }
